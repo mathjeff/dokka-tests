ae8619cfe208fc20eeab0d7a45efaf1cbe07f6a4
Jeff Gaston <jeffrygaston@google.com>
2018-02-14 18:53:48 -0500

    example of confusing error when android lib depends on java lib

---
+++ unclear-error-when-applying-java-plugin-to-dependency/jefflib1/src/main/java/com/jefflib/JeffLib.java
@@ -0,0 +1,13 @@
+package com.jefflib;
+
+//import androidx.app.ActivityCompat;
+//import android.support.v4.app.ActivityCompat;
+
+/**
+ * Class JeffLib. Sample class.
+ */
+public final class JeffLib {
+  public static void testme() {
+    System.out.println("testing");
+  }
+}
